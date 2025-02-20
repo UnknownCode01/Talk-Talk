f387f00c59717faadd56d8d96a1c9a05bde5461e
Brijit Adak <brijitadak000@gmail.com>
2025-02-20 18:54:45 +0530

    add

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import VideoFeed from "./components/VideoFeed";
+import { IVideo } from "@/models/Video";
+import { apiClient } from "@/lib/api-client";
+
+export default function Home() {
+  const [videos, setVideos] = useState<IVideo[]>([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const data = await apiClient.getVideos();
+        setVideos(data);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }
+    };
+
+    fetchVideos();
+  }, []);
+
+  return (
+    <main className="container mx-auto px-4 py-2 flex justify-center">
+      <VideoFeed videos={videos} />
+    </main>
+  );
+}
